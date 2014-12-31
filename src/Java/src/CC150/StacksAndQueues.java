05d46a974663117c463aeabbc297648a6bca4d22
Tangent Chang <tien.jen.chang@sv.cmu.edu>
2014-12-30 17:24:31 -0800

    finishied 3.3

--- src/Java/src/CC150/StacksAndQueues.java
+++ src/Java/src/CC150/StacksAndQueues.java
@@ -1,52 +1,105 @@
 package Java.src.CC150;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
  * Created by YHWH on 12/28/14.
  */
 public class StacksAndQueues {
 
-    public class Node{
-        Node next = null;
-        Object data = null;
-        public Node(Object item){ data = item;}
+    public class MinStack extends Stack<Integer> {
+        Stack<Integer> s;
 
-        public void appendToTail(Object item){
-            Node end = new Node(item);
-            Node n = this;
-            while(n.next!=null){
-                n = n.next;
-            }
-            n.next = end;
+        public MinStack(){ s = new Stack<Integer>();}
+
+        public void push(int value){
+            if(value <= min()){ s.push(value);}
+            super.push(value);
         }
 
-        public Node getNext(){ return this.next;}
-        public Object getData(){ return this.data;}
-        public void setNext(Node n){ this.next = n;}
+        public Integer pop(){
+            int value = super.pop();
+            if(value == min()){ s.pop();}
+            return value;
+        }
+
+        public int min(){
+            if(s.isEmpty()){ return Integer.MAX_VALUE;}
+            else{ return s.peek();}
+        }
     }
 
-    public class Stack{
-        Node top;
-        Object pop(){
-            if(top != null){
-                Object item = top.data;
-                top = top.next;
-                return item;
-            }
-            return null;
+    public Stack hanoi(int diskNum){
+        Stack<Integer> originTower = new Stack<Integer>();
+        Stack<Integer> bufferTower = new Stack<Integer>();
+        Stack<Integer> goalTower = new Stack<Integer>();
+
+        for(int i = diskNum; i >= 1; i--){
+            originTower.push(i);
+        }
+        moveDisks(diskNum, originTower, goalTower, bufferTower);
+
+        return goalTower;
+    }
+    private void moveDisks(int diskNum, Stack<Integer> originTower, Stack<Integer> goalTower, Stack<Integer> bufferTower){
+
+        if(diskNum>0){
+            moveDisks(diskNum-1, originTower, bufferTower, goalTower);
+            int disk = originTower.pop();
+            goalTower.push(disk);
+            System.out.println(originTower + " , " + bufferTower + " , " + goalTower);
+            moveDisks(diskNum - 1, bufferTower, goalTower, originTower);
         }
 
-        void push(Object item){
-            Node t = new Node(item);
-            t.next = top;
-            top = t;
-        }
-
-        Object peek(){ return top.data;}
     }
 
-    public class MinStack extends Stack{
-        Stack s;
+    public class SetOfStacks{
 
+        ArrayList <Stack> stacks = new ArrayList<Stack>();
+        int capacity;
+        int last = 0;
+
+        public SetOfStacks(int c){
+            Stack s = new Stack();
+            stacks.add(s);
+            capacity = c;
+        }
+
+        public int pop(){
+            int value =(Integer) stacks.get(last).pop();
+            if(stacks.get(last).size() == 0){
+                stacks.remove(last);
+                last = last - 1;
+            }
+            return value;
+        }
+        public void push(int item){
+            if(stacks.get(last).size() == capacity){
+                last = last + 1;
+                stacks.add(new Stack());
+            }
+            stacks.get(last).push(item);
+        }
+
+        public int getSize(){ return stacks.size();}
+
+        public int popAt(int index){
+            if(index<0 || index >= stacks.size()){ return 0;}
+
+            int value = (Integer) stacks.get(index).pop();
+            int current = index;
+            while(current+1<stacks.size()){
+                stacks.get(current).push(stacks.get(current+1).pop());
+                current = current+1;
+            }
+            if(stacks.get(last).size() == 0){
+                stacks.remove(last);
+                last = last - 1;
+            }
+
+            return value;
+        }
 
     }
 
--- test/Java/test/CC150/StacksAndQueuesTest.java
+++ test/Java/test/CC150/StacksAndQueuesTest.java
@@ -1,12 +1,53 @@
 package Java.test.CC150;
 
+import Java.src.CC150.LinkedLists;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import Java.src.CC150.StacksAndQueues;
 import Java.src.CC150.StacksAndQueues.*;
 
+import java.util.Stack;
+
 /**
  * Created by YHWH on 12/28/14.
  */
 public class StacksAndQueuesTest {
+
+    StacksAndQueues Test = new StacksAndQueues();
+
+    @Test
+    public void minStackTest(){
+        MinStack s = Test.new MinStack();
+        s.push(5);
+        s.push(9);
+        assertEquals(5, s.min());
+        s.push(4);
+        s.push(2);
+        assertEquals(2, s.min());
+        s.pop();
+        assertEquals(4, s.min());
+
+    }
+
+    @Test
+    public void hanoiTest(){ Stack result = Test.hanoi(3);}
+
+    @Test
+    public void SetOfStacksTest(){
+        SetOfStacks stack = Test.new SetOfStacks(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        assertEquals(2, stack.getSize());
+        stack.pop();
+        assertEquals(1, stack.getSize());
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.push(8);
+        stack.popAt(4);
+        assertEquals(3, stack.getSize());
+    }
 }
